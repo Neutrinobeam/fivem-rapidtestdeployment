f4f07c7920015200d8b9b3a71723941217bac2aa
Neutrinobeam <40643118+Neutrinobeam@users.noreply.github.com>
2018-07-08 00:03:36 -0500

    Update __resource.lua
    Add server script and update version number.

--- rapidtestdeployment/__resource.lua
+++ rapidtestdeployment/__resource.lua
@@ -3,12 +3,18 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 files {
 	'index.html',
 	'ui.js',
-	'ui.css'	
+	'ui.css'
 }
 
 ui_page 'index.html'
 
-client_script 'clientRTD.js'
+client_scripts {
+	'clientRTD.js'
+}
+
+server_scripts {
+	'serverRTD.js'
+}
 
 description 'Rapid Test Deployment'
-version '1.1'
+version '1.2'
